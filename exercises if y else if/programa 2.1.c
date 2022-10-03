0e8e4a06c122ce05a479e2f2564c2e2fa597e4a9
Juliodelarosap <juliodelarp@gmail.com>
2022-10-02 20:04:07 -0400

    Create programa 2.1.c

---
+++ exercises if y else if/programa 2.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* promedio curso.
+el programa, al recibir como dato el promedio de un alumno de un curso
+universitario, escribe aprobado si su promedio es mayor o igual a 6.
+
+PRO: variable de tipo real.  */
+
+void main(void)
+{
+float PRO; // variable de coma flotante se usa float
+printf("ingrese el promedio del alumno: ");
+scanf("%f", &PRO);
+if (PRO >= 6)// aqui se dice que si el promedio es mayor o igual o 6 que imprima aprobado 
+	printf("\naprobado");
+}
